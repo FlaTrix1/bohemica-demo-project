bb2632b6ceae76547753ab6267cf424c0425bdd1
Martin Dékány <dekany.martin@gmail.com>
2023-10-06 13:42:58 +0200

    grid fix

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,5 +1,8 @@
 <template>
-	<div v-if="products.length > 0" class="grid grid-cols-3 gap-3">
+	<div
+		v-if="products.length > 0"
+		class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3"
+	>
 		<ProductItem v-for="product in products" :product="product" />
 	</div>
 	<div v-else class="flex text-gray-600 justify-center">
